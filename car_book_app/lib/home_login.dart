e9a3d826252c0131a4aa67bb764d4c9c885cdb11
CoderSleek <dnarula7jan@gmail.com>
2022-07-25 00:53:12 +0530

    day3

--- car_book_app/lib/home_login.dart
+++ car_book_app/lib/home_login.dart
@@ -5,32 +5,55 @@ import 'package:car_book_app/utils/routes.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  static final _isvalid = RegExp(r'^[a-zA-Z0-9]+$');
-
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
   String _uid = "";
-
   String _pas = "";
+  final _isvalid = RegExp(r'^[a-zA-Z0-9]+$');
 
   bool changeButton =
       false; //use for both changestate and next page load if info valid
 
-  bool _validUid() {
-    if (_uid.isNotEmpty && LoginPage._isvalid.hasMatch(_uid)) {
-      return true;
+  final _loginFormKey = GlobalKey<FormState>();
+
+  // bool _validUid() {
+  //   if (_uid.isNotEmpty && _isvalid.hasMatch(_uid)) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
+
+  // bool _validPas() {
+  //   if (_pas.isNotEmpty && _isvalid.hasMatch(_pas)) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
+
+  void validateLogin() async {
+    if (_loginFormKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(const Duration(milliseconds: 5000));
+      // ignore: use_build_context_synchronously
+      // Navigator.pushReplacementNamed(context, MyRoutes.startPage);
     }
-    return false;
   }
 
-  bool _validPas() {
-    if (_pas.isNotEmpty && LoginPage._isvalid.hasMatch(_pas)) {
-      return true;
+  String? validateInputs(String? value, String item, int numOfChars) {
+    if (value == null) return null;
+    if (value.isEmpty) {
+      return "$item must not be empty";
+    } else if (value.length < numOfChars) {
+      return "$item cannot be less than $numOfChars charachter";
+    } else if (!_isvalid.hasMatch(value)) {
+      return "$item cannot contain special charachters";
     }
-    return false;
+    return null;
   }
 
   @override
@@ -58,54 +81,80 @@ class _LoginPageState extends State<LoginPage> {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-              child: Column(
-                children: [
-                  TextFormField(
-                    maxLength: 30,
-                    onChanged: (val) => {_uid = val},
-                    decoration: const InputDecoration(
-                      hintText: "Enter Employee ID",
-                      labelText: "User ID",
+              child: Form(
+                key: _loginFormKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      maxLength: 30,
+                      onChanged: (val) => {_uid = val},
+                      decoration: const InputDecoration(
+                        hintText: "Enter Employee ID",
+                        labelText: "User ID",
+                      ),
+                      validator: (value) {
+                        return validateInputs(value, "ID", 0);
+                      },
                     ),
-                  ),
-                  TextFormField(
-                    maxLength: 30,
-                    onChanged: (val) => {_pas = val},
-                    decoration: const InputDecoration(
-                      hintText: "Enter Employee password",
-                      labelText: "Password",
+                    TextFormField(
+                      maxLength: 30,
+                      onChanged: (val) => {_pas = val},
+                      decoration: const InputDecoration(
+                        hintText: "Enter Employee password",
+                        labelText: "Password",
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        return validateInputs(value, "Password", 6);
+                      },
                     ),
-                    obscureText: true,
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             ElevatedButton(
-              onPressed: () async {
-                setState(() {
-                  if (_validPas() && _validUid()) {
-                    changeButton = true;
-                  } else {}
-                });
-                await Future.delayed(const Duration(milliseconds: 500));
-                if (changeButton) {
-                  // ignore: use_build_context_synchronously
-                  Navigator.pushNamed(context, MyRoutes.startPage);
-                }
-              },
+              onPressed: validateLogin,
               style: ElevatedButton.styleFrom(
-                minimumSize: const Size(140, 40),
-                // primary: Colors.amber, text color
-                shadowColor: const Color.fromRGBO(00, 44, 255, 1.0),
-                // backgroundColor: Colors.black, btn clr
-                shape:
-                    changeButton ? const CircleBorder() : const StadiumBorder(),
-                padding: changeButton
-                    ? const EdgeInsets.all(11)
-                    : const EdgeInsets.all(0),
-              ),
+                  minimumSize: const Size(140, 40),
+                  // primary: Colors.amber, text color
+                  // surfaceTintColor: Colors.yellow,
+                  shadowColor: const Color.fromRGBO(00, 44, 255, 1.0),
+                  // backgroundColor: Colors.black, btn clr 4 textbutton
+                  shape: changeButton
+                      ? const CircleBorder()
+                      : const StadiumBorder(),
+                  padding: changeButton
+                      ? const EdgeInsets.all(11)
+                      : const EdgeInsets.all(0),
+                  animationDuration: const Duration(milliseconds: 500)),
               child:
                   changeButton ? const Icon(Icons.done) : const Text("Login"),
+            ),
+            Material(
+              color: Colors.teal,
+              // animationDuration: const Duration(microseconds: 1),
+              borderRadius: BorderRadius.circular(100), //needs to be same as *1
+              child: InkWell(
+                radius: 600, //speed
+                borderRadius: BorderRadius.circular(100), //*1
+                // focusColor: Colors.white,
+                splashColor: Colors.blue.shade500, //holdcolor
+                highlightColor: Colors.blue.shade700, //fullholdcolor
+                onTap: () async {
+                  setState(() {});
+                },
+                child: AnimatedContainer(
+                  // color: Colors.greenAccent,
+                  duration: const Duration(seconds: 1),
+                  width: 150,
+                  height: 40,
+                  alignment: Alignment.center,
+                  child: const Icon(
+                    Icons.pets,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
