9635b4df498cc0a0f427b13b7b9c3732363bf24f
CoderSleek <dnarula7jan@gmail.com>
2022-08-08 15:43:31 +0530

    day17

--- website/app.js
+++ website/app.js
@@ -3,16 +3,21 @@ async function verifyLogin(){
     const pass = document.getElementById('pass').value;
 
     try{
-        let response = await fetch(backend_url+'admincredcheck',
-            {method: 'POST', headers:{'Content-Type':'application/json'}, 
-            body: JSON.stringify({'uname': uname,'password':pass})});
+        // let response = await fetch(backend_url+'admincredcheck',
+        //     {method: 'POST', headers:{'Content-Type':'application/json'}, 
+        //     body: JSON.stringify({'uname': uname,'password':pass})});
 
-        let token = await response.json()
+        // let token = await response.json()
+        let token = true;
+
         // console.log(body)
 
 
         if(token){
-            window.location.href = backend_url+'adminpage?token='+token;
+            // window.location.href = backend_url+'adminpage?token='+token;
+            console.log('fetching')
+            fetch(backend_url+'adminpage', {method: 'get', headers:{'Content-Type':'application/json'},
+            body:JSON.stringify({"token":"randomstuff"})})
         }
     } catch (err) {
         alert('Some error Occured')
